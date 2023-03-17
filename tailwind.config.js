610d2c93d82b406f892b9ddfefd10ade7183ee4d
Genimarcio <49964829+Genimarcio@users.noreply.github.com>
2023-03-17 07:21:20 -0300

    telaInicial.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         'color_Geral': '#1E1E1E',
         'color_Letras': '#7e7e7e',
         'color_Fundos': 'rgba(203, 234, 227, 0.1);',
+        'color_HTML': '#475ABF',
+        'color_CSS': '#43A1E5',
+        'color_REACT': '#FF3535',
+        'color_buscaTAG': '#24FF00',
       },
 
       fontFamily: {
